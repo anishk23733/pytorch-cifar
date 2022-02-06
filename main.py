03d77303d5b8c6865d286194d1ada8ad09739611
Anish Kachinthaya <anishk23733@gmail.com>
2022-02-06 14:25:12 -0800

    updated for wandb

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import argparse
 
 from models import *
 from utils import progress_bar
+import wandb
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -20,6 +21,8 @@ parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
+
+wandb.init(project="pytorch-start-pack", entity="anish1")
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -42,19 +45,19 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
+net = VGG('VGG19')
 # net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
@@ -68,7 +71,7 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
+# net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -88,6 +91,17 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
+wandb.config = {
+  "learning_rate": args.lr,
+  "epochs": 200,
+  "batch_size": 128
+}
+
+wandb.watch(net, args, log="all", log_freq=10)
+
+best_model_checkpoint = wandb.Artifact('best_model_checkpoint', type='model')
+best_model_checkpoint.add_file('./checkpoint/ckpt.pth')
+run = wandb.init(project="pytorch-start-pack", entity="anish1", job_type="model-training")
 
 # Training
 def train(epoch):
@@ -97,6 +111,7 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
+        print(batch_idx)
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -111,7 +126,11 @@ def train(epoch):
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
+        wandb.log({
+            "train_loss": loss, # or train_loss? train_loss looks to aggregate all the losses
+            "train_accuracy": 100.*correct/total,
+            "step": epoch * 128 + batch_idx,
+        })
 
 def test(epoch):
     global best_acc
@@ -132,6 +151,11 @@ def test(epoch):
 
             progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            wandb.log({
+                "test_loss": loss, # or train_loss? train_loss seems to aggregate
+                "test_accuracy": 100.*correct/total,
+                "step": epoch * 128 + batch_idx,
+            })
 
     # Save checkpoint.
     acc = 100.*correct/total
@@ -146,7 +170,10 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
-
+        wandb.run.summary["best_accuracy"] = best_acc
+        wandb.run.summary["best_accuracy_epoch"] = epoch
+        run.log_artifact(best_model_checkpoint)
+        best_model_checkpoint.wait()
 
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
